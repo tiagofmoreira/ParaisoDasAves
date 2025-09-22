e15321d0969810dec70f3feefae4250811d431c5
Tiago Francisco Moreira <tiagoojipa@gmail.com>
2025-09-22 11:37:17 -0400

    .

--- obj/Debug/net8.0/AppExemplo.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppExemplo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppExemplo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0264aa958a95524fb0e0977865a4ba10290baebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppExemplo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppExemplo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
